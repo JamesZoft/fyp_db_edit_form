b76586ea28e387507c26ec62ef74a240748434b2
Jarob22 <james@jamesreed.name>
2013-05-05 13:32:18 +0100

    small fix to match and tourny entry controllers

--- app/controllers/match_entry_controller.rb
+++ app/controllers/match_entry_controller.rb
@@ -23,16 +23,18 @@ before_filter :authenticate_user!
 		if @matchEntry.update_attributes(params[:match_entry])
 		  redirect_to(@matchEntry)
 		  subscriptions = Subscription.all
-		  subscriptions.each { |subscription| 
-	    if(subscription.match_entry.id == @matchEntry.id)
-	      new_notification = Gcm::Notification.new
-	      new_notification.device = subscription.device
-	      new_notification.collapse_key = "new_match_data_available"
-	      new_notification.delay_while_idle = true
-	      new_notification.data = {:registration_ids => [subscription.device.registration_id], :data => {:message_text => @matchEntry}}
-	      new_notification.save
-	    end
-            }
+		  subscriptions.each do |subscription| 
+		    if(subscription != nil)
+	        if(subscription.match_entry.id == @matchEntry.id)
+	          new_notification = Gcm::Notification.new
+	          new_notification.device = subscription.device
+	          new_notification.collapse_key = "new_match_data_available"
+	          new_notification.delay_while_idle = true
+	          new_notification.data = {:registration_ids => [subscription.device.registration_id], :data => {:message_text => @matchEntry}}
+	          new_notification.save
+	        end
+	      end
+      end
 	    Gcm::Notification.send_notifications
 		else
 		  render :edit
